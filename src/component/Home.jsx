61f191ada02b28585a50bf6031f2e81b90b7474b
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-10-25 15:13:46 +0100

    add twitter link

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,13 +1,26 @@
-import profile_img from '../assets/profile_img.svg';
+import profile_img from "../assets/profile_img.svg";
 
 const Home = () => {
-    return (
-        <>
-        <div className="home">
-            <img className="profile_img" src={profile_img} alt="profile_img" />
-            <div className='name'>Olamide Deborah</div>
+  return (
+    <>
+      <div className="home">
+        <img className="profile_img" src={profile_img} alt="profile_img" />
+        <div className="name">Olamide Deborah</div>
+        <div className="links-section">
+          <div className="link-background">
+            <a href="https://twitter.com/OlamideTehillah" id="twitter">
+              Twitter Link
+            </a>
+          </div>
+          <button className="link-background">
+            <a href="https://twitter.com/OlamideTehillah" id="btn__zuri">
+              Zuri Team
+            </a>
+          </button>
+
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 export default Home;

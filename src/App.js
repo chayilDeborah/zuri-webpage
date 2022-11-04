bba55ae65083cd45e67a958d5194392fb0a65270
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-11-04 14:42:38 +0100

    feat: add contact page routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Contact from './component/Contact';
 import Home from './component/Home';
 
 const App = () =>  {
@@ -8,6 +9,7 @@ const App = () =>  {
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
     </Router>
     </>

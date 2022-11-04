c4494d9a7db4a8a6d8617eb7522d72b67a218bd4
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-11-04 15:30:39 +0100

    feat: create contact me heading and subheading

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,7 +1,14 @@
 const Contact = () => {
     return (
         <>
-        contact page
+        <div className="contact-wrap">
+            <div className="contact-div">
+                <div className="contact-me">
+                    <div className="title">Contact Me</div>
+                    <p className="subtitle">Hi there, contact me to ask me about anything you have in mind.</p>
+                </div>
+            </div>
+        </div>
         </>
     )
 }

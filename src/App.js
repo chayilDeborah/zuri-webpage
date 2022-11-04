830fad615b7c5c420db6de65e4e26a97ad0c1090
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-11-04 13:35:58 +0100

    feat: import react router dom into App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './component/Home';
 
-function App() {
+const App = () =>  {
   return (
     <>
-    <Home />
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
     </>
   );
 }

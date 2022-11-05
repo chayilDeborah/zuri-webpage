afb42b56cae38d059e3efd138bfc21535e7bdf01
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-11-05 19:21:22 +0100

    refactor: modify footer component

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,9 +1,21 @@
 import '../App.css';
+import zuri from "../assets/zuri.svg";
+import I4G from "../assets/I4G.svg";
 
 const Footer = () => {
     return (
         <>
-        HHHHH
+        <div className="footer">
+            <div className="footer-div">
+              <div className="footer-items">
+                <img src={zuri} alt="zuri" className="zuri-svg" />
+                <div className="hng-text">
+                  <div>HNG Internship 9 Frontend Task</div>
+                </div>
+                <img src={I4G} alt="i4g" className="i4g"/>
+              </div>
+            </div>
+          </div>
         </>
     )
 }
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -2,8 +2,6 @@ import profile_img from "../assets/profile_img.svg";
 import slack from "../assets/slack.svg";
 import share from "../assets/share.svg";
 import github from "../assets/github.svg";
-import zuri from "../assets/zuri.svg";
-import I4G from "../assets/I4G.svg";
 import dots from '../assets/dots.svg';
 import { Link } from "react-router-dom";
 import Footer from "./Footer";
@@ -84,17 +82,6 @@ const Home = () => {
               <a href="https://github.com/chayilDeborah/zuri-webpage">
               <img src={github} alt="github_img" className="github" />
               </a>
-            </div>
-          </div>
-          <div className="footer">
-            <div className="footer-div">
-              <div className="footer-items">
-                <img src={zuri} alt="zuri" className="zuri-svg" />
-                <div className="hng-text">
-                  <div>HNG Internship 9 Frontend Task</div>
-                </div>
-                <img src={I4G} alt="i4g" className="i4g"/>
-              </div>
             </div>
           </div>
           <Footer />
